41c2957ab2293d24551f1cc2ebcda8270cb9e3d5
Mateusz Drożdż <mateusz212@gmail.com>
2015-05-11 23:05:51 +0200

    Webapp: dropbox  directories browsing

--- src/main/webapp/static/app/controllers/dropboxCloudManagementController.js
+++ src/main/webapp/static/app/controllers/dropboxCloudManagementController.js
@@ -8,16 +8,18 @@
         .controller('DropboxCloudManagementController', function ($scope, $log, dropboxCloudManagementFactory, initialDirectoryPath) {
 
             $scope.files = null;
-            $scope.currentDirectoryPath = null;
+            $scope.currentDirectoryPath = initialDirectoryPath;
+            $scope.parentsPaths = [];
 
             $scope.listFiles = function (directoryPath) {
-                $scope.currentDirectoryPath = directoryPath;
                 dropboxCloudManagementFactory.listFiles({
                     "path": directoryPath
                 })
                 .success(function (response) {
                     console.log('DropboxCloudManagementController - listFiles - success.');
                     $scope.files = response.result;
+                    $scope.parentsPaths.push($scope.currentDirectoryPath);
+                    $scope.currentDirectoryPath = directoryPath;
                 })
                 .error(function (error) {
                     console.log('DropboxCloudManagementController - listFiles - error.');
@@ -25,6 +27,30 @@
                     $log.log(error);
                 });
             };
+
+
+            $scope.goBack = function () {
+                console.log("goBack()")
+                if ($scope.parentsPaths.length > 0) {
+                    parentPath = $scope.parentsPaths.pop();
+                    dropboxCloudManagementFactory.listFiles({
+                        "path": parentPath
+                    })
+                        .success(function (response) {
+                            console.log('DropboxCloudManagementController - goBack - success.');
+                            $scope.files = response.result;
+                            $scope.currentDirectoryPath = parentPath;
+                        })
+                        .error(function (error) {
+                            console.log('DropboxCloudManagementController - goBack - error.');
+                            $scope.errors = error
+                            $log.log(error);
+                        });
+                } else {
+                    console.log("Already in the root");
+                }
+            };
+
 
             if ($scope.files === null) {
                 $scope.listFiles(initialDirectoryPath);
