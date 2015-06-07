5edac98d0eb14bc716cd843d343a8a14411902e7
Pawel Mikolajczyk <pawel.b.mikolajczyk@gmail.com>
2015-06-07 02:23:02 +0200

    Develop integration test for Onedrive.

--- src/test/java/pl/edu/agh/iosr/cloud/it/OnedriveIT.java
+++ src/test/java/pl/edu/agh/iosr/cloud/it/OnedriveIT.java
@@ -1,87 +1,71 @@
 package pl.edu.agh.iosr.cloud.it;
 
-import com.google.common.base.Function;
-import com.google.common.collect.FluentIterable;
+import com.sun.jersey.api.client.Client;
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import pl.edu.agh.iosr.cloud.common.CloudType;
 import pl.edu.agh.iosr.cloud.common.files.CloudPath;
 import pl.edu.agh.iosr.cloud.common.files.FileMetadata;
-import pl.edu.agh.iosr.cloud.common.session.BasicSession;
-import pl.edu.agh.iosr.cloud.common.tasks.ProgressAwareFuture;
+import pl.edu.agh.iosr.cloud.common.session.CloudSessionStatus;
 import pl.edu.agh.iosr.cloud.onedrive.services.OnedriveCloudManagementService;
-import pl.edu.agh.iosr.cloud.onedrive.services.OnedriveCloudSessionService;
+import pl.edu.agh.iosr.cloud.onedrive.sessionswtf.OnedriveCloudSession;
 
-import javax.annotation.Nullable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.concurrent.Executors;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath*:**/applicationConfig.xml" })
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class OnedriveIT {
 
-    private final String authorizationCode = "M33dd0035-bba7-8dde-9229-897d0b9d9338";
+    private final String accessToken = System.getProperty("test.onedrive.token");
 
-    @Autowired
-    private OnedriveCloudSessionService sessionService;
-    @Autowired
     private OnedriveCloudManagementService managementService;
-    private BasicSession session;
+    private OnedriveCloudSession session;
 
     @Before
     public void setUp() throws Exception {
-        session = sessionService.loginUser("santiago", authorizationCode);
+        managementService = new OnedriveCloudManagementService(Executors.newSingleThreadExecutor(), new Client());
+        session = new OnedriveCloudSession("someCode", accessToken, CloudSessionStatus.ACTIVE);
     }
 
-    @Ignore
+    //TODO: extract to some shared place cause this is copy-paste
+
     @Test
     public void testEndToEnd() throws Exception {
-//        managementService.deleteFile(session.getSessionId(), new CloudPath("/__E2E_TEST_FILE", CloudType.ONE_DRIVE)).get();
-//
-//        List<FileMetadata> listed = managementService.listAllDirectoryFiles(session.getSessionId(), new CloudPath("/", CloudType.ONE_DRIVE)).get();
-//        for (FileMetadata fileMetadata : listed) {
-//            assertThat(fileMetadata.getFileName()).isNotEqualTo("__E2E_TEST_FILE");
-//        }
-//
-//        managementService.uploadFile(session.getSessionId(), new CloudPath("/__E2E_TEST_FILE", CloudType.ONE_DRIVE), "", createInputStream()).get();
-//
-//        List<FileMetadata> listedAfterUpload = managementService.listAllDirectoryFiles(session.getSessionId(), new CloudPath("/", CloudType.ONE_DRIVE)).get();
-//        assertThat(FluentIterable.from(listedAfterUpload).transform(metadataIntoFilename())).contains("__E2E_TEST_FILE", CloudType.ONE_DRIVE);
-//
-//        Boolean deleted = managementService.deleteFile(session.getSessionId(), new CloudPath("/__E2E_TEST_FILE", CloudType.ONE_DRIVE)).get();
-//        assertThat(deleted).isTrue();
-//
-//        List<FileMetadata> listedAfterDelete = managementService.listAllDirectoryFiles(session.getSessionId(), new CloudPath("/", CloudType.ONE_DRIVE)).get();
-//        for (FileMetadata fileMetadata : listedAfterDelete) {
-//            assertThat(fileMetadata.getFileName()).isNotEqualTo("__E2E_TEST_FILE");
-//        }
-//
-//        assertThat(true).isTrue();
+        managementService.deleteFile(session, new CloudPath("/__E2E_TEST_FILE", CloudType.ONE_DRIVE)).get();
+
+        List<FileMetadata> listed = managementService.listAllDirectoryFiles(session, new CloudPath("/", CloudType.ONE_DRIVE)).get();
+        assertThat(listed).extracting(FileMetadata::getFileName).doesNotContain("__E2E_TEST_FILE");
+
+        managementService.uploadFile(session, new CloudPath("/__E2E_TEST_FILE", CloudType.ONE_DRIVE), "ABSOLUTELY_NOT_IMPORTANT_THING", createInputStreamWithText("hello world")).get();
+
+        List<FileMetadata> listedAfterUpload = managementService.listAllDirectoryFiles(session, new CloudPath("/", CloudType.ONE_DRIVE)).get();
+        assertThat(listedAfterUpload).extracting(FileMetadata::getFileName).contains("__E2E_TEST_FILE");
+
+        PipedOutputStream outputStream = new PipedOutputStream();
+        PipedInputStream grabbedDataStream = new PipedInputStream(outputStream);
+        managementService.downloadFile(session, new CloudPath("/__E2E_TEST_FILE", CloudType.ONE_DRIVE), outputStream).get();
+        //TODO: close in task
+        outputStream.close();
+        assertThat(IOUtils.toString(grabbedDataStream)).isEqualTo("hello world");
+
+        Boolean deleted = managementService.deleteFile(session, new CloudPath("/__E2E_TEST_FILE", CloudType.ONE_DRIVE)).get();
+        assertThat(deleted).isTrue();
+
+        List<FileMetadata> listedAfterDelete = managementService.listAllDirectoryFiles(session, new CloudPath("/", CloudType.ONE_DRIVE)).get();
+        assertThat(listedAfterDelete).extracting(FileMetadata::getFileName).doesNotContain("__E2E_TEST_FILE");
     }
 
-    private Function<FileMetadata, String> metadataIntoFilename() {
-        return new Function<FileMetadata, String>() {
-            @Nullable
-            @Override
-            public String apply(FileMetadata fileMetadata) {
-                return fileMetadata.getFileName();
-            }
-        };
-    }
-
-    private InputStream createInputStream() throws IOException {
+    private InputStream createInputStreamWithText(String text) throws IOException {
         PipedInputStream givenContentStream = new PipedInputStream();
         PipedOutputStream outputStream = new PipedOutputStream(givenContentStream);
-        outputStream.write("hello world".getBytes(StandardCharsets.US_ASCII));
+        outputStream.write(text.getBytes(StandardCharsets.US_ASCII));
         outputStream.close();
 
         return givenContentStream;
